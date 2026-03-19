2039a9e9924f5d484341c3bdb72693323f113782
hongsoonchan02 <asm640789@gmail.com>
2026-03-19 14:42:57 +0900

    react-crud-upload

--- src/hooks/useNoticeEditHook.js
+++ src/hooks/useNoticeEditHook.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import { useNoticeDetailQuery } from "../query/NoticeDetailQuery";
+import { useNoticeDetailQuery } from "./query/NoticeDetailQuery";
 
 
 export const useNoticeEditHook = () => {
